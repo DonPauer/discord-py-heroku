1f25ed819eedc5c4943e43366b9e632ff36f786f
DonPauer <gordon.vidal@gmx.at>
2020-10-28 16:19:17 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -23,24 +23,25 @@ def line(width):
 def quote(s):
  return "‏‏‎ ‎"+s+"‏‏‎ ‎"
 
+
 def generate(targets=6,small=False, wide=False):
 
-  height = 15
-  width = 200 if wide else 100 
+  height = 30
+  width = 300 if wide else 150 
     
   lines = [line(width) for x in range(height)]
 
   while targets > 0:
     r_i = random.randint(0, height-1)
-    r_j = random.randint(0, width-1)
+    r_j = random.randint(1, width-2)
     if lines[r_i][r_j] != " ":
       continue
-    lines[r_i][r_j] = "||p||" if small else "||pop||"
+    lines[r_i][r_j] = "||p||" if small else "o"
+    lines[r_i][r_j+1] = lines[r_i][r_j+1] if small else "p||"
+    lines[r_i][r_j-1] = lines[r_i][r_j-1] if small else "||p"
     targets -= 1
   lines = ["".join(x) for x in lines]
   return "\n".join(map(quote, lines))
-
-print(generate(targets=10,small=True))
 
 
 
