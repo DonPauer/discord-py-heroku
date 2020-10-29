b62cc7034650290e0767ff27fe5ac448ba29b584
DonPauer <gordon.vidal@gmx.at>
2020-10-30 00:11:42 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -50,5 +50,38 @@ def generate(targets=6,small=False, wide=False):
 async def aimtrain(ctx, numOne: int):
     await ctx.send(generate(targets=numOne))
 
+
+def generate2(targets=6,small=False, wide=False):
+
+  height = 30
+  width = 300 if wide else 100 
+    
+  lines = [line(width) for x in range(height)]
+
+  while targets > 0:
+    r_i = random.randint(0, height-1)
+    r_j = random.randint(1, width-2)
+    if lines[r_i][r_j] != " ":
+      continue
+    lines[r_i][r_j] = "||p||" if small else "o"
+    lines[r_i][r_j+1] = lines[r_i][r_j+1] if small else "p||"
+    lines[r_i][r_j-1] = lines[r_i][r_j-1] if small else "||p"
+    targets -= 1
+  lines = ["".join(x) for x in lines]
+  return "\n".join(map(quote, lines))
+
+def splitMsg(msg):
+    splits = msg.split("\n")
+    threeLineArray = [[splits[3*i], splits[3*i + 1],splits[3*i+2]] 
+      for i in range(len(splits)//3)]
+    threeLineArray = ["".join(x) for x in threeLineArray]
+    return threeLineArray
+
+
+@bot.command()
+async def ww6t(ctx, numOne: int):
+    for msg in generate2(targets=6, wide=True)
+        await ctx.send(msg)
+
 if __name__ == "__main__":
     bot.run(TOKEN)
